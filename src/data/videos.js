faa7868db846715b0929c46cc532f0f17922f6cc
OzzyD1 <ozzydomarkas@gmail.com>
2025-01-09 14:48:34 +0000

    All clips are now added and branch navigation is working

--- src/data/videos.js
+++ src/data/videos.js
@@ -7,21 +7,20 @@ const videos = [
   },
   {
     id: 2,
-    title: "Start of Promotion Arc",
+    title: "Message The Manager (Start Promotion Arc)",
     src: "/assets/animatic/Promotion Arc/Act 3 - Promotion Arc.mp4",
     choices: [4, 5]
   },
   {
     id: 3,
-    title: "Start of Discovery Arc",
-    src: "/assets/animatic/Discovery Arc/Act 4 - Discovery Arc.mp4",
-    choices: []
+    title: "Repair (Start Discovery Arc)",
+    src: "/assets/animatic/Discovery Arc/Act 3 - Discovery Arc.mp4",
+    choices: [6, 7]
   },
   {
     id: 4,
-    title: "Promotion Loopback",
+    title: "Message The Manager (Promotion Loopback)",
     src: "/assets/animatic/Promotion Arc/Act 3.a - Promotion Loopback.mp4",
-    choices: [],
     loopbackTo: {
       videoId: 1,
       timestamp: 37 // in seconds
@@ -29,10 +28,35 @@ const videos = [
   },
   {
     id: 5,
-    title: "Promotion Continue",
-    src: "/assets/animatic/Promotion Arc/Act 3.b - Continue Promotion Arc.mp4",
+    title: "Don't Tell The Manager (Continue Promotion Arc)",
+    src: "/assets/animatic/Promotion Arc/Act 4-5 - Promotion Arc.mp4",
     choices: []
   },
+  {
+    id: 6,
+    title: "Restart Simulation (Discovery Loopback)",
+    src: "assets/animatic/Discovery Arc/Act 3.a - Discovery Loopback.mp4",
+    loopbackTo: {
+      videoId: 1,
+      timestamp: 37 // in seconds
+    },
+  },
+  {
+    id: 7,
+    title: "Continue Digging (Continue Discovery Arc)",
+    src: "assets/animatic/Discovery Arc/Act 4 - Discovery Arc.mp4",
+    choices: [8, 9]
+  },
+  {
+    id: 8,
+    title: "Ignore The Manager, exit the simulation",
+    src: "assets/animatic/Discovery Arc/Act 5 - Discovery Arc (Leave).mp4",
+  },
+  {
+    id: 9,
+    title: "Reply to The Manager, stay the simulation",
+    src: "assets/animatic/Discovery Arc/Act 5 - Discovery Arc (Stay).mp4",
+  }
 ];
 
 export default videos;
