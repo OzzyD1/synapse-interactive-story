c80895fcacb25c12aad7f21e702d1a7d354c1a77
OzzyD1 <ozzydomarkas@gmail.com>
2025-03-02 22:36:54 +0000

    Fixed file paths

--- src/data/videos.js
+++ src/data/videos.js
@@ -2,28 +2,28 @@ const videos = [
     {
         id: 1,
         title: "Act 1 - 2",
-        src: "/videos/act1-2.mp4",
+        src: "videos/act1-2.mp4",
         choices: [2, 3],
         promptStart: 15,
     },
     {
         id: 2,
         title: "Message The Manager",
-        src: "/videos/promotion-arc/act3-1-promotion.mp4",
+        src: "videos/promotion-arc/act3-1-promotion.mp4",
         choices: [4, 5],
         promptStart: 10,
     },
     {
         id: 3,
         title: "Repair",
-        src: "/videos/discovery-arc/act3-1-discovery.mp4",
+        src: "videos/discovery-arc/act3-1-discovery.mp4",
         choices: [6, 7],
         promptStart: 12,
     },
     {
         id: 4,
         title: "Message The Manager",
-        src: "/videos/promotion-arc/act3-loop-promotion.mp4",
+        src: "videos/promotion-arc/act3-loop-promotion.mp4",
         choices: [],
         loopbackTo: {
             videoId: 1,
@@ -33,13 +33,13 @@ const videos = [
     {
         id: 5,
         title: "Don't Tell The Manager",
-        src: "/videos/promotion-arc/act3-2-5-promotion.mp4",
+        src: "videos/promotion-arc/act3-2-5-promotion.mp4",
         choices: [],
     },
     {
         id: 6,
         title: "Restart Simulation",
-        src: "/videos/discovery-arc/act3-loop-discovery.mp4",
+        src: "videos/discovery-arc/act3-loop-discovery.mp4",
         choices: [],
         loopbackTo: {
             videoId: 1,
@@ -49,20 +49,20 @@ const videos = [
     {
         id: 7,
         title: "Continue Digging",
-        src: "/videos/discovery-arc/act3-2-4-discovery.mp4",
+        src: "videos/discovery-arc/act3-2-4-discovery.mp4",
         choices: [8, 9],
         promptStart: 20,
     },
     {
         id: 8,
         title: "Leave",
-        src: "/videos/discovery-arc/act5-leave.mp4",
+        src: "videos/discovery-arc/act5-leave.mp4",
         choices: [],
     },
     {
         id: 9,
         title: "Stay",
-        src: "/videos/discovery-arc/act5-stay.mp4",
+        src: "videos/discovery-arc/act5-stay.mp4",
         choices: [],
     },
 ];
