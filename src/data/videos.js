aca6f3c4aee822218aa911ddd2aef9d6636eeed6
OzzyD1 <ozzydomarkas@gmail.com>
2025-03-02 21:29:54 +0000

    Updated file names and paths

--- src/data/videos.js
+++ src/data/videos.js
@@ -2,67 +2,67 @@ const videos = [
     {
         id: 1,
         title: "Act 1 - 2",
-        src: "/synapse-interactive-story/videos/Act 1-2.mp4",
+        src: "/videos/act1-2.mp4",
         choices: [2, 3],
-        promptStart: 15, // Prompt will appear 15 seconds before the end
+        promptStart: 15,
     },
     {
         id: 2,
         title: "Message The Manager",
-        src: "/synapse-interactive-story/videos/Promotion Arc/Act 3.1 Promotion Arc.mp4",
+        src: "/videos/promotion-arc/act3-1-promotion.mp4",
         choices: [4, 5],
         promptStart: 10,
     },
     {
         id: 3,
         title: "Repair",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 3.1 Discovery Arc.mp4",
+        src: "/videos/discovery-arc/act3-1-discovery.mp4",
         choices: [6, 7],
         promptStart: 12,
     },
     {
         id: 4,
         title: "Message The Manager",
-        src: "/synapse-interactive-story/videos/Promotion Arc/Act 3.L Promotion Arc.mp4",
+        src: "/videos/promotion-arc/act3-loop-promotion.mp4",
         choices: [],
         loopbackTo: {
             videoId: 1,
-            timestamp: 65, // in seconds
+            timestamp: 65,
         },
     },
     {
         id: 5,
         title: "Don't Tell The Manager",
-        src: "/synapse-interactive-story/videos/Promotion Arc/Act 3.2 - 5 Promotion Arc.mp4",
+        src: "/videos/promotion-arc/act3-2-5-promotion.mp4",
         choices: [],
     },
     {
         id: 6,
         title: "Restart Simulation",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 3.L Discovery Arc.mp4",
+        src: "/videos/discovery-arc/act3-loop-discovery.mp4",
         choices: [],
         loopbackTo: {
             videoId: 1,
-            timestamp: 65, // in seconds
+            timestamp: 65,
         },
     },
     {
         id: 7,
         title: "Continue Digging",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 3.2 - 4 Discovery Arc.mp4",
+        src: "/videos/discovery-arc/act3-2-4-discovery.mp4",
         choices: [8, 9],
         promptStart: 20,
     },
     {
         id: 8,
         title: "Leave",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 5 - Leave.mp4",
+        src: "/videos/discovery-arc/act5-leave.mp4",
         choices: [],
     },
     {
         id: 9,
         title: "Stay",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 5 - Stay.mp4",
+        src: "/videos/discovery-arc/act5-stay.mp4",
         choices: [],
     },
 ];
