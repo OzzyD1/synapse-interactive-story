08b15fc88c75995e7368da61f44045ec215093e9
OzzyD1 <ozzydomarkas@gmail.com>
2025-03-02 21:15:14 +0000

    Updated video file names

--- src/data/videos.js
+++ src/data/videos.js
@@ -2,28 +2,28 @@ const videos = [
     {
         id: 1,
         title: "Act 1 - 2",
-        src: "/synapse-interactive-story/videos/Act1-2.mp4",
+        src: "/synapse-interactive-story/videos/Act 1-2.mp4",
         choices: [2, 3],
         promptStart: 15, // Prompt will appear 15 seconds before the end
     },
     {
         id: 2,
         title: "Message The Manager",
-        src: "/synapse-interactive-story/videos/Promotion Arc/Act 3 - Promotion Arc.mp4",
+        src: "/synapse-interactive-story/videos/Promotion Arc/Act 3.1 Promotion Arc.mp4",
         choices: [4, 5],
         promptStart: 10,
     },
     {
         id: 3,
         title: "Repair",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 3 - Discovery Arc.mp4",
+        src: "/synapse-interactive-story/videos/Discovery Arc/Act 3.1 Discovery Arc.mp4",
         choices: [6, 7],
         promptStart: 12,
     },
     {
         id: 4,
-        title: "Message The Manager (Promotion Loopback)",
-        src: "/synapse-interactive-story/videos/Promotion Arc/Act 3.a - Promotion Loopback.mp4",
+        title: "Message The Manager",
+        src: "/synapse-interactive-story/videos/Promotion Arc/Act 3.L Promotion Arc.mp4",
         choices: [],
         loopbackTo: {
             videoId: 1,
@@ -32,14 +32,14 @@ const videos = [
     },
     {
         id: 5,
-        title: "Don't Tell The Manager (Continue Promotion Arc)",
-        src: "/synapse-interactive-story/videos/Promotion Arc/Act 4-5 - Promotion Arc.mp4",
+        title: "Don't Tell The Manager",
+        src: "/synapse-interactive-story/videos/Promotion Arc/Act 3.2 - 5 Promotion Arc.mp4",
         choices: [],
     },
     {
         id: 6,
-        title: "Restart Simulation (Discovery Loopback)",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 3.a - Discovery Loopback.mp4",
+        title: "Restart Simulation",
+        src: "/synapse-interactive-story/videos/Discovery Arc/Act 3.L Discovery Arc.mp4",
         choices: [],
         loopbackTo: {
             videoId: 1,
@@ -48,21 +48,21 @@ const videos = [
     },
     {
         id: 7,
-        title: "Continue Digging (Continue Discovery Arc)",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 4 - Discovery Arc.mp4",
+        title: "Continue Digging",
+        src: "/synapse-interactive-story/videos/Discovery Arc/Act 3.2 - 4 Discovery Arc.mp4",
         choices: [8, 9],
         promptStart: 20,
     },
     {
         id: 8,
-        title: "Ignore The Manager, exit the simulation",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 5 - Discovery Arc (Leave).mp4",
+        title: "Leave",
+        src: "/synapse-interactive-story/videos/Discovery Arc/Act 5 - Leave.mp4",
         choices: [],
     },
     {
         id: 9,
-        title: "Reply to The Manager, stay the simulation",
-        src: "/synapse-interactive-story/videos/Discovery Arc/Act 5 - Discovery Arc (Stay).mp4",
+        title: "Stay",
+        src: "/synapse-interactive-story/videos/Discovery Arc/Act 5 - Stay.mp4",
         choices: [],
     },
 ];
